049e6c2e320bc21d577df86a538d009d3fe036cb
collins lenjo <collinslenjo.cl@gmail.com>
2017-08-27 18:04:40 +0300

    making of the video player

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from 'react-dom'
+// Create a New Component. This Component Should produse
+//some HTML
+const App = () => {
+	return <div>Hi!</div>
+}
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Take this Component's html and put it
+//on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
